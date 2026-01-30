a908b67917012301babf9151bae0405f40d16ca5
Nathan Silva de Souza <nathan_bzss@hotmail.com>
2026-01-29 23:26:56 -0300

    Tela de cadastro

---
+++ app/src/main/java/com/example/planner/ui/UserRegistrationFragment.kt
@@ -0,0 +1,39 @@
+package com.example.planner.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.planner.R
+import com.example.planner.databinding.FragmentUserRegistrationBinding
+
+class UserRegistrationFragment : Fragment() {
+
+    private var _binding: FragmentUserRegistrationBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            // TODO Logica da tela de cadastro do usuario
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
